c0f363811f77a0a35b7880a09bbedf7836423119
Bang <hbtruong.2017@sis.smu.edu.sg>
2020-06-06 18:44:35 +0800

    Implement Singly LInked List

---
+++ LinkedList/SinglyLinkedList.py
@@ -0,0 +1,30 @@
+class Node: 
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+
+    def __init__(self):
+        self.head = None 
+
+    # traverse the linkedlist
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+
+    linked_list.head = Node(1) 
+    second = Node(2)
+    third = Node(3)
+
+    linked_list.head.next = second
+    second.next = third
+
+    linked_list.printList()
+    
